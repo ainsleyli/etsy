07a5bd3911de8464e5c4372106f25f74936fa957
ainsleyli <yuling.li7@gmail.com>
2020-09-26 15:57:43 -0400

    array filej

---
+++ TestAsset/empData.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+        Name: 'Jalapeno Pupper',
+        Number: '1234557890',
+        Email: 'abc@live.com',
+        Title: 'CBO',
+    },
+
+    {
+        Name: 'Habenero Pepper',
+        Number: '8571234567',
+        Email: 'efg@live.com',
+        Title: 'Vegetable'
+    }
+]
