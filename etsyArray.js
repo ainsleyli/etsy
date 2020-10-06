2087d37e6fc14e0d099d7ae163ac1aaf46b62175
ainsleyli <71519331+ainsleyli@users.noreply.github.com>
2020-10-06 19:07:47 -0400

    array file for etsy

---
+++ etsyArray.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {   number: '2', category: 'Jewelry & Accessories' },
+    {   number: '3', category: 'Clothing & Shoes' },
+    {   number: '4', category: 'Home & Living' },
+    {   number: '5', category: 'Wedding & Party'},
+    {   number: '6', category: 'Toys & Entertainment' },
+    {   number: '7', category: 'Art & Collectibles'},
+    {   number: '8', category: 'Craft Supplies & Tools' },
+
+
+]
